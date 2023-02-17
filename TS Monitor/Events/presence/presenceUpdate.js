58fdec8ccc7a9b478016939bbadc551ad4c8babc
Thunder <79953877+ThunderDoesDev@users.noreply.github.com>
2023-02-18 04:52:59 +1000

    Add files via upload

---
+++ TS Monitor/Events/presence/presenceUpdate.js
@@ -0,0 +1,56 @@
+const {
+    EmbedBuilder,
+    time,
+    Colors
+} = require('discord.js');
+const { log, error } = require("../../Functions/logger")
+
+module.exports.run = (oldPresence, newPresence) => {
+    if (!oldPresence || !newPresence) return;
+    if (oldPresence.status === newPresence.status) return;
+    if (newPresence.userId !== oldPresence.settings.bot.botID) return;
+    const bot = oldPresence.users.cache.get(oldPresence.settings.bot.botID);
+    const channel = newPresence.guild.channels.cache.find(chan => chan.id === oldPresence.settings.bot.statusChannel) || null;
+    if (channel !== null) {
+        if (newPresence.status === 'offline') {
+            const embed = new EmbedBuilder()
+                .setTitle(`${oldPresence.user.username} - Status Monitor`)
+                .setColor(Colors.Grey)
+                .setDescription(`**${bot.username} Is Currently Offline.**`)
+                .addFields({
+                    name: `Incident Date/Time:`,
+                    value: `${time(new Date(), 'R')}`
+                })
+                .setThumbnail('https://cdn.discordapp.com/attachments/506233661616816138/513747774638915584/offline.png')
+            channel.send({
+                embeds: [embed]
+            }).then(() => log(`STATUS`, `${bot.username} Is Currently Offline.`)).catch(e => error(`ERROR`, e));
+        } else if (newPresence.status === 'online') {
+            const embed = new EmbedBuilder()
+                .setTitle(`${oldPresence.user.username} - Status Monitor`)
+                .setColor(Colors.Green)
+                .setDescription(`**${bot.username} is now back Online.**`)
+                .addFields({
+                    name: `Incident Date/Time:`,
+                    value: `${time(new Date(), 'R')}`
+                })
+                .setThumbnail('https://cdn.discordapp.com/attachments/506233661616816138/513747775947669506/online.png')
+            channel.send({
+                embeds: [embed]
+            }).then(() => log(`STATUS`, `${bot.username} is now back Online.`)).catch(e => error(`ERROR`, e));
+        } else if (newPresence.status === 'dnd') {
+            const embed = new EmbedBuilder()
+                .setTitle(`${oldPresence.user.username} - Status Monitor`)
+                .setColor(Colors.Red)
+                .setDescription(`**${bot.username} has gone down for Maintenance.**`)
+                .addFields({
+                    name: `Incident Date/Time:`,
+                    value: `${time(new Date(), 'R')}`
+                })
+                .setThumbnail('https://cdn.discordapp.com/attachments/506233661616816138/513747772848209921/dnd.png')
+            channel.send({
+                embeds: [embed]
+            }).then(() => log(`STATUS`, `${bot.username} has gone down for Maintenance.`)).catch(e => error(`ERROR`, e));
+        }
+    }
+}
